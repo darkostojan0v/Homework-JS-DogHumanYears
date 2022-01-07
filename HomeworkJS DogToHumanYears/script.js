8792a3390862699e3dc774d96460fee61963efe8
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-07 16:58:30 +0100

    Add files via upload

---
+++ HomeworkJS DogToHumanYears/script.js
@@ -0,0 +1,6 @@
+console.log('Calculate your dog years');
+
+let inputHumanYears = prompt("How old is your dog?");
+let dogYears = inputHumanYears * 7;
+
+console.log(`Your dog is ${dogYears} years old !`);
